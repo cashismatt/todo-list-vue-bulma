58cd758112eb8921e4a7cb2b8c1e58510e4cc8a7
Joshua Echeverri <josheche@gmail.com>
2017-07-31 12:16:17 -0400

    init app started new component template for todolist

---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <ul>
+      <li>
+        Todo 1
+      </li>
+      <li>
+        Todo 2
+      </li>
+      <li>
+        Todo 3
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script type="text/javascript">
+  export default {
+
+  };
+</script>
+
+<style>
+  
+</style>
